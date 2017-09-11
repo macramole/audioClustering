67195540cbf84957ea4804c816c273579351a70d
leandro <leandrogarber@gmail.com>
2017-09-11 09:29:25 -0300

    mas

--- imagePCAimage.py
+++ imagePCAimage.py
@@ -1,22 +1,24 @@
 from scipy import misc
 import numpy as np
+from sklearn.decomposition import PCA
 
-IMAGE_WIDTH = 740
-IMAGE_HEIGHT = 960
-
-image = misc.imread("data/img/2.png")
+#image = misc.imread("data/img/2.png")
+image = misc.imread("data/img/zorro.resized.2.jpg")
 print(image.shape)
+
+IMAGE_WIDTH = image.shape[1]
+IMAGE_HEIGHT = image.shape[0]
+
 #lo paso a matriz, tipo dataset
-imageReshaped = image.reshape((100,-1))
+imageReshaped = image.reshape((300,-1))
 print(imageReshaped.shape)
 
-# pca_components = 5
+#pca_components = 1
 
 #for pca_components in range(1,100):
 for pca_components in range(1,100):
     #hago PCA
     print("")
-    from sklearn.decomposition import PCA
     pca = PCA(n_components=pca_components)
     print("PCA...")
     pca.fit(imageReshaped)
--- main.py
+++ main.py
@@ -8,24 +8,23 @@ from math import ceil
 import time
 
 import librosa
+import librosa.display
+import librosa.core
 
 import matplotlib
 import matplotlib.pyplot as plt
 
+import sounddevice as sd
+
 #%% Load Audio
 
 SAMPLE_RATE = 22050
-SIZE_STFT = 45100 #esto seguramente se pueda calcular pero bue
-
 SEGUNDOS_FILA = 1
-SELECCION_DIR = "data/else/"
-FILE_TYPE=".mp3"
+SIZE_AUDIO_RAW = ceil(SAMPLE_RATE * SEGUNDOS_FILA)
 
-def doSTFT(data):
-    D = librosa.stft(data)
-    D = np.abs(D)
-#    return D.reshape(1, D.shape[0] * D.shape[1] )
-    return D.reshape(D.shape[0] * D.shape[1] )
+
+SELECCION_DIR = "data/sound/pack/drumkits.mp3/"
+FILE_TYPE=".mp3"
 
 def findMusic(directory):
     musicFiles = []
@@ -48,14 +47,84 @@ audioFiles = findMusic( SELECCION_DIR )
 print("")
 print("Encontré",len(audioFiles),"archivos de audio")    
 
+#%% Procesos
 
-#%% FFT Process
+SIZE_STFT = 45100 #esto seguramente se pueda calcular pero bue
+def doSTFT(data):
+    D = librosa.stft(data)
+    D = np.abs(D)
+#    return D.reshape(1, D.shape[0] * D.shape[1] )
+    return D.reshape(D.shape[0] * D.shape[1] )
 
-import sys
+SIZE_ZERO_CROSSING_RATE = 22 #esto seguramente se pueda calcular pero bue
+def getZeroCrossingRate(data):
+    zc = librosa.feature.zero_crossing_rate(data)
+    return zc.reshape( zc.shape[0] * zc.shape[1] )
+
+SIZE_RMSE = 22 #esto seguramente se pueda calcular pero bue
+def getRMSE(data):
+    rmse = librosa.feature.rmse(data)
+    return rmse.reshape( rmse.shape[0] * rmse.shape[1] )
+
+SIZE_MFCC = 440 #esto seguramente se pueda calcular pero bue
+def getMFCC(data):
+    mfcc = librosa.feature.mfcc(data, sr = SAMPLE_RATE)
+    return mfcc.reshape( mfcc.shape[0] * mfcc.shape[1] )
+
+
+
+#%% Testing
+
+#testAudioData, _ = librosa.core.load(audioFiles[0], sr = SAMPLE_RATE)
+testAudioData, _ = librosa.core.load( audioFiles[round( np.random.uniform(0, len(audioFiles)) )], sr = SAMPLE_RATE)
+sd.play( testAudioData )
+testAudioData = testAudioData[0:SIZE_AUDIO_RAW]
+#RMSE
+testRMSE = librosa.feature.rmse( testAudioData )
+plt.semilogy(testRMSE.T, label='RMS Energy')
+plt.xticks([])
+plt.xlim([0, testRMSE.shape[-1]])
+plt.legend(loc='best')
+    
+#STFT
+testSTFT = librosa.stft( testAudioData )
+testSTFTdb = librosa.amplitude_to_db(testSTFT, ref=np.max)
+#plt.subplot(4, 2, 1)
+librosa.display.specshow(testSTFTdb, y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Log-frequency power spectrogram')
+
+#MFCC
+testMFCC = librosa.feature.mfcc( testAudioData, sr = SAMPLE_RATE )
+librosa.display.specshow(testMFCC, x_axis='time')
+plt.colorbar()
+plt.title('MFCC')
+plt.tight_layout()
+
+#Mel spectogram
+testMelSpectogram = librosa.feature.melspectrogram(testAudioData, SAMPLE_RATE)
+librosa.display.specshow( librosa.core.power_to_db(testMelSpectogram , ref=np.max), y_axis='mel', fmax=8000,x_axis='time')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Mel spectrogram')
+plt.tight_layout()
+
+#Zero crossing
+testZeroCrossing = librosa.feature.zero_crossing_rate(testAudioData)
+
+#RMSE
+testRMSE = librosa.feature.rmse( testAudioData )
+plt.semilogy(testRMSE.T, label='RMS Energy')
+plt.xticks([])
+plt.xlim([0, testRMSE.shape[-1]])
+plt.legend(loc='best')
+
+
+#%% Audio Process
 
 #matrixAudioDataNoFFT = np.empty((0,ceil(SAMPLE_RATE * SEGUNDOS_FILA)), np.int16)
 #matrixAudioData = np.empty((0, SIZE_STFT), np.float32)
-matrixAudioData = np.empty((0, SIZE_STFT + (SAMPLE_RATE * SEGUNDOS_FILA) ), np.float32)
+#matrixAudioData = np.empty((0, SIZE_STFT + SIZE_AUDIO_RAW ), np.float32)
+matrixAudioData = np.empty((0, SIZE_STFT + SIZE_AUDIO_RAW ), np.float32)
 
 count = 0
 COUNT_NOTICE = 200
@@ -68,7 +137,7 @@ for file in audioFiles:
 
         tmpAudioData, tmpSampleRate = librosa.core.load(file, sr = SAMPLE_RATE)
         
-        tmpAudioData.resize(ceil(SAMPLE_RATE * SEGUNDOS_FILA))
+        tmpAudioData.resize(SIZE_AUDIO_RAW)
 
         stft = doSTFT(tmpAudioData)
     
